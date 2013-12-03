a06ee76b9970a25d4fa11aa3221068bc5dacb9a7
Vlado Cingel <vladocingel@gmail.com>
2013-12-03 23:43:47 +0100

    Dotenv added to easily load env var

--- open_exchange_rates.gemspec
+++ open_exchange_rates.gemspec
@@ -19,4 +19,5 @@ Gem::Specification.new do |gem|
 
   gem.add_development_dependency('rr')
   gem.add_development_dependency('rake')
+  gem.add_development_dependency('dotenv')
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,16 +1,11 @@
 require "rubygems"
 require "test/unit"
 require "rr"
+require 'dotenv'
 require "open_exchange_rates"
 
 # Pick up ENV variables from .env file if exists
-dot_env_file_path = File.expand_path("../../.env", __FILE__)
-if File.exist?(dot_env_file_path)
-  File.open(dot_env_file_path).each_line do |line|
-    key, value = line.strip.split("=")
-    ENV[key] = value unless key.nil? || value.nil?
-  end
-end
+Dotenv.load
 
 OpenExchangeRates.configuration.app_id = ENV['OPEN_EXCHANGE_RATES_APP_ID']
 
